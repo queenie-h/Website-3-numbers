7b8cdfb358350d13d56cab4742fcfd7b360645d9
queenieh <queeniehornsby@gmail.com>
2024-11-03 18:36:22 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+prompt("Enter a number:");
+
+
+if (number > 0) {
+    console.log("The number is positive.");
+} else if (number < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
